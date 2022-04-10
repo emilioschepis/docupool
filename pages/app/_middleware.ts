6235d455a732702c0ab403b8203d3d8736ce5e12
Emilio Schepis <emilio.schepis@gmail.com>
2022-04-10 11:32:10 +0200

    fix: middleware auth status

--- pages/app/_middleware.ts
+++ pages/app/_middleware.ts
@@ -2,9 +2,19 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import supabase from "../../lib/supabase";
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const { user } = await supabase.auth.api.getUserByCookie(req);
+  const { user, token } = await supabase.auth.api.getUserByCookie(req);
 
-  if (!user) {
+  if (!user || !token) {
+    const url = req.nextUrl.clone();
+    url.pathname = `/login`;
+    url.searchParams.append("redirectedFrom", req.nextUrl.pathname);
+    return NextResponse.redirect(url);
+  }
+
+  const payloadBuffer = Buffer.from(token.split(".")[1], "base64");
+  const payload = JSON.parse(payloadBuffer.toString());
+
+  if (!payload.exp || payload.exp < new Date().getTime()) {
     const url = req.nextUrl.clone();
     url.pathname = `/login`;
     url.searchParams.append("redirectedFrom", req.nextUrl.pathname);
