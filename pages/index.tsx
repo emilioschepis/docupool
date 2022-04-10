d6c2020431851ef592fe593f506c8120bd137305
Emilio Schepis <emilio.schepis@gmail.com>
2022-04-10 19:21:11 +0200

    feat: redirect to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Index: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => void router.replace("/app"), [router]);
+
   return <Box />;
 };
 
